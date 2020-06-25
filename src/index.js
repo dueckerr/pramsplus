6ce968a8c205dbd1271696e7596a74a07359e7fd
Reagan Duecker <44736698+dueckerr@users.noreply.github.com>
2020-06-25 11:46:29 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { createBrowserHistory } from "history";
+// import { Router, Route, Switch } from "react-router-dom";
 
-import "assets/scss/material-kit-react.scss?v=1.9.0";
+// import "assets/scss/material-kit-react.scss?v=1.9.0";
 
-// pages for this product
-import Home from "views/Home/Home.js";
-import Solutions from "views/Solutions/Solutions.js";
-import Blog from "views/Content/Blog.js";
-import About from "views/About/About.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
+// // pages for this product
+// import Home from "views/Home/Home.js";
+// import Solutions from "views/Solutions/Solutions.js";
+// import Blog from "views/Content/Blog.js";
+// import About from "views/About/About.js";
+// import LoginPage from "views/LoginPage/LoginPage.js";
 
-var hist = createBrowserHistory();
+// var hist = createBrowserHistory();
 
-ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/solutions" component={Solutions} />
-      <Route path="/content" component={Blog} />
-      <Route path="/login-page" component={LoginPage} />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <Router history={hist}>
+//     <Switch>
+//       <Route path="/" component={Home} />
+//       <Route path="/about" component={About} />
+//       <Route path="/solutions" component={Solutions} />
+//       <Route path="/content" component={Blog} />
+//       <Route path="/login-page" component={LoginPage} />
+//     </Switch>
+//   </Router>,
+//   document.getElementById("root")
+// );
 
 
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
