db80e1ea5c34c4444dcd0cccfcea5b1fde6c0718
Reagan D Duecker <dueckerr@gmail.com>
2020-06-26 14:18:43 -0600

    work

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { createBrowserHistory } from "history";
-// import { Router, Route, Switch } from "react-router-dom";
-
-// import "assets/scss/material-kit-react.scss?v=1.9.0";
-
-// // pages for this product
-// import Home from "views/Home/Home.js";
-// import Solutions from "views/Solutions/Solutions.js";
-// import Blog from "views/Content/Blog.js";
-// import About from "views/About/About.js";
-// import LoginPage from "views/LoginPage/LoginPage.js";
-
-// var hist = createBrowserHistory();
-
-// ReactDOM.render(
-//   <Router history={hist}>
-//     <Switch>
-//       <Route path="/" component={Home} />
-//       <Route path="/about" component={About} />
-//       <Route path="/solutions" component={Solutions} />
-//       <Route path="/content" component={Blog} />
-//       <Route path="/login-page" component={LoginPage} />
-//     </Switch>
-//   </Router>,
-//   document.getElementById("root")
-// );
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
