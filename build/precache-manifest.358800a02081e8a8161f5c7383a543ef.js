fdfe181d3b535edfcafb0a9a3d23b7d4bcb98c5a
Reagan D Duecker <dueckerr@gmail.com>
2020-06-26 14:04:31 -0600

    builds

---
+++ build/precache-manifest.358800a02081e8a8161f5c7383a543ef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06981fbe1e11db9521f1554f701ee325",
+    "url": "/pramsplus/index.html"
+  },
+  {
+    "revision": "5cb3badbd6f1b5153c67",
+    "url": "/pramsplus/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "abc11c85b00a109c1b3e",
+    "url": "/pramsplus/static/js/2.4a933ebb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/pramsplus/static/js/2.4a933ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb3badbd6f1b5153c67",
+    "url": "/pramsplus/static/js/main.89e7fd7a.chunk.js"
+  },
+  {
+    "revision": "5781477d7fb3c0fdb270",
+    "url": "/pramsplus/static/js/runtime-main.d363cbd6.js"
+  },
+  {
+    "revision": "7e768577281857b1b717af054d206049",
+    "url": "/pramsplus/static/media/apple-icon.7e768577.png"
+  },
+  {
+    "revision": "df2fc999f932f487de8e6d9247806c77",
+    "url": "/pramsplus/static/media/bbl.df2fc999.png"
+  },
+  {
+    "revision": "6a54344913104b30cf52ec306e4808bd",
+    "url": "/pramsplus/static/media/chart.6a543449.png"
+  },
+  {
+    "revision": "88c9dd1d302f038e85c4c0d24904327d",
+    "url": "/pramsplus/static/media/grey.88c9dd1d.png"
+  },
+  {
+    "revision": "fd81f9178242b54ff63388924a1cb888",
+    "url": "/pramsplus/static/media/logo.fd81f917.png"
+  },
+  {
+    "revision": "4bef13a222bb4ed31a49fa7a2a6e3176",
+    "url": "/pramsplus/static/media/pump.4bef13a2.png"
+  }
+]);
