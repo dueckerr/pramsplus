6578223a43d7e7426206bad2e58eca00a29f3ad0
Reagan D Duecker <dueckerr@gmail.com>
2020-06-27 12:18:45 -0600

    Updates

--- precache-manifest.236e2b12c9a75968fa12090b0942bc19.js
+++ precache-manifest.236e2b12c9a75968fa12090b0942bc19.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58f81836b87d34d179b8ebce4a3552eb",
+    "revision": "6d2206700cf8b7014874427029a4c24f",
     "url": "/pramsplus/index.html"
   },
   {
-    "revision": "7f6e331031bda2225b36",
+    "revision": "d729cca614b87e24c607",
     "url": "/pramsplus/static/css/main.d373da39.chunk.css"
   },
   {
-    "revision": "93d0a26c940b0bdbb9d3",
-    "url": "/pramsplus/static/js/2.cfdc29ab.chunk.js"
+    "revision": "9f2f04a48d85b3949ca7",
+    "url": "/pramsplus/static/js/2.ca09e492.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/pramsplus/static/js/2.cfdc29ab.chunk.js.LICENSE.txt"
+    "url": "/pramsplus/static/js/2.ca09e492.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7f6e331031bda2225b36",
-    "url": "/pramsplus/static/js/main.83c06534.chunk.js"
+    "revision": "d729cca614b87e24c607",
+    "url": "/pramsplus/static/js/main.026aca35.chunk.js"
   },
   {
     "revision": "5781477d7fb3c0fdb270",
     "url": "/pramsplus/static/js/runtime-main.d363cbd6.js"
+  },
+  {
+    "revision": "770b6e759dd9d16fb114dff0cd518383",
+    "url": "/pramsplus/static/media/HomePage.770b6e75.png"
   },
   {
     "revision": "7e768577281857b1b717af054d206049",
@@ -36,6 +40,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pramsplus/static/media/chart.6a543449.png"
   },
   {
+    "revision": "e2975ccc06cce661ca27626ec5942d4b",
+    "url": "/pramsplus/static/media/dailyPumperX2.e2975ccc.png"
+  },
+  {
     "revision": "88c9dd1d302f038e85c4c0d24904327d",
     "url": "/pramsplus/static/media/grey.88c9dd1d.png"
   },
@@ -46,5 +54,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "4bef13a222bb4ed31a49fa7a2a6e3176",
     "url": "/pramsplus/static/media/pump.4bef13a2.png"
+  },
+  {
+    "revision": "a236156fdf8da2adbac9491194e20ead",
+    "url": "/pramsplus/static/media/pushReport.a236156f.png"
   }
 ]);
