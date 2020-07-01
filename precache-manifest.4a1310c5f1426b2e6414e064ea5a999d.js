0d3e80370d8e2a505b34264b042edab3cc0e284d
Reagan D Duecker <dueckerr@gmail.com>
2020-07-01 15:25:50 -0600

    Updates

---
+++ precache-manifest.4a1310c5f1426b2e6414e064ea5a999d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766bba4b796dca4e8f44b8b489ac6683",
+    "url": "/pramsplus/index.html"
+  },
+  {
+    "revision": "c94a7e6907253f808f09",
+    "url": "/pramsplus/static/css/main.d373da39.chunk.css"
+  },
+  {
+    "revision": "528f4dce0bd69124d09a",
+    "url": "/pramsplus/static/js/2.e828feaa.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/pramsplus/static/js/2.e828feaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94a7e6907253f808f09",
+    "url": "/pramsplus/static/js/main.58000736.chunk.js"
+  },
+  {
+    "revision": "5781477d7fb3c0fdb270",
+    "url": "/pramsplus/static/js/runtime-main.d363cbd6.js"
+  },
+  {
+    "revision": "770b6e759dd9d16fb114dff0cd518383",
+    "url": "/pramsplus/static/media/HomePage.770b6e75.png"
+  },
+  {
+    "revision": "7e768577281857b1b717af054d206049",
+    "url": "/pramsplus/static/media/apple-icon.7e768577.png"
+  },
+  {
+    "revision": "f1abb78f1626eee3b958f3635d9e3d2a",
+    "url": "/pramsplus/static/media/assetMgmt.f1abb78f.jpeg"
+  },
+  {
+    "revision": "3241129b3216522a9172e3edf08bf869",
+    "url": "/pramsplus/static/media/connectedOilField.3241129b.jpeg"
+  },
+  {
+    "revision": "e2975ccc06cce661ca27626ec5942d4b",
+    "url": "/pramsplus/static/media/dailyPumperX2.e2975ccc.png"
+  },
+  {
+    "revision": "ddaf87a0dbc012ce7ebb301859e3e438",
+    "url": "/pramsplus/static/media/datacharts2.ddaf87a0.png"
+  },
+  {
+    "revision": "0e665ccfc447b0921f606002d3cae72f",
+    "url": "/pramsplus/static/media/fieldDataCapture.0e665ccf.jpeg"
+  },
+  {
+    "revision": "88c9dd1d302f038e85c4c0d24904327d",
+    "url": "/pramsplus/static/media/grey.88c9dd1d.png"
+  },
+  {
+    "revision": "99786ae44997ba2a1c01056936360ab6",
+    "url": "/pramsplus/static/media/landingPumps.99786ae4.jpeg"
+  },
+  {
+    "revision": "fd81f9178242b54ff63388924a1cb888",
+    "url": "/pramsplus/static/media/logo.fd81f917.png"
+  },
+  {
+    "revision": "a236156fdf8da2adbac9491194e20ead",
+    "url": "/pramsplus/static/media/pushReport.a236156f.png"
+  },
+  {
+    "revision": "8b120401baffcef279179f2fd89ae652",
+    "url": "/pramsplus/static/media/refinery.8b120401.jpeg"
+  }
+]);
